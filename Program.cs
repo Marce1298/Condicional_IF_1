8782a1d2878d0ef4afd8dce4662e0655042271b9
Marce1298 <marchebordon@gmail.com>
2020-09-24 00:31:07 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 25;
+
+            Console.WriteLine("vamos a evaluar si eres mayor de edad");
+
+            if (edad >= 18) 
+            {
+
+                Console.WriteLine("Adelante, puedes pasar porque eres mayor de edad");
+
+            }
+
+        }
+    }
+}
